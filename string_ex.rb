bf1c1f72fe3f4716a89e4961670aedcb9827d795
udeshika Priyadarshani <hello.udeshi@gmail.com>
2023-11-17 12:38:34 -0500

    add string exercises

---
+++ string_ex.rb
@@ -0,0 +1,52 @@
+# #a = ''
+# a= String.new
+# puts a 
+
+#puts "It's now 12 o'clock."
+
+# ##Ignoring case
+# name = 'Roger'
+# puts name.casecmp('roger')== 0
+# puts name.casecmp('regor')== 0
+
+##Dynamic Printing 
+# name = 'Elizabeth'
+
+# puts "Hello, #{name}!"
+
+# first_name = 'John'
+# last_name = 'Doe'
+# full_name = first_name+ ' '+last_name
+# puts full_name
+
+# ##Tricky Formatting
+# state = 'tExAs'
+# puts state.capitalize to mutate sate variable use "state.capitalized!"
+
+# ##Goodbye, not hello
+# greeting = 'Hello!'
+# puts greeting.gsub!('Hello!', 'Good Bye!')
+# puts greeting
+
+# ##Print Alphabet
+# alphabet = 'abcdefghijklmnopqrstuvwxyz'
+# puts alphabet.split('') 
+
+# ##Pluralize
+# words = 'car human elephant airplane'
+#  words.split(' ').each do |word|
+#     puts word + 's'
+#  end
+
+# ##Are you There?
+# colors = 'blue pink yellow orange'
+# puts colors.match?(/yellow/)
+# puts colors.match?(/purple/)
+
+# puts colors.include?('yellow')
+# puts colors.include?('purple')
+
+
+
+
+
